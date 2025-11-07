42bc94e62b17d47c56b9f1ef06fc192d2890009e
thoma <thomas.deltour@efrei.net>
2025-11-07 14:43:08 +0100

    PARTIE 1 done

--- main.c
+++ main.c
@@ -2,24 +2,25 @@
 #include "graph.h"
 
 int main(void) {
-    const char *path = "../data/exemple1.txt"; // adapte si besoin
+    const char *path = "../data/exemple3.txt"; // adapte si besoin
 
     AdjList G = readGraph(path);
 
-    printf("=== Étape 1 : Affichage de la liste d'adjacence ===\n");
+    printf("=== Etape 1 : Affichage de la liste d'adjacence ===\n");
     adj_print(&G);
 
-    printf("\n=== Étape 2 : Vérification du graphe de Markov ===\n");
+    printf( "\n=== Etape 2 : Verification du graphe de Markov ===\n");
     if (adj_is_markov(&G)) {
         printf("Le graphe est un graphe de Markov.\n");
     } else {
         printf("Le graphe n'est PAS un graphe de Markov.\n");
     }
 
-    printf("\n=== Étape 3 : Génération du fichier Mermaid ===\n");
+    printf("\n=== Etape 3 : Generation du fichier Mermaid ===\n");
     adj_to_mermaid(&G, "graph_mermaid.txt");
-    printf("Fichier 'graph_mermaid.txt' généré avec succès !\n");
+    printf("Fichier 'graph_mermaid.txt' genere avec succes !\n");
 
     adj_free(&G);
     return 0;
 }
+
